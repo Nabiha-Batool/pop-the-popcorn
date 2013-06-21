1132f8a904d312420f2a6a207280eb7ae1497b6f
Nabiha Batool <great.nabiha@gmail.com>
2013-06-21 23:43:36 +0500

    try version of player

---
+++ hi/README.txt
@@ -0,0 +1,13 @@
+hi README
+==================
+
+Getting Started
+---------------
+
+- cd <directory containing this file>
+
+- $venv/bin/python setup.py develop
+
+- $venv/bin/populate_hi development.ini
+
+- $venv/bin/pserve development.ini
---
+++ hi/hi/controllers/views.py
@@ -0,0 +1,29 @@
+from pyramid.view import view_config
+from pyramid.httpexceptions import HTTPFound
+
+from ..models import (
+    DBSession,
+    #Site,
+    )
+
+from ..forms import ContactForm
+
+@view_config(route_name='home', renderer='player.mako')
+def my_view(request):
+    #one = DBSession.query(Site).filter(Site.name=='one').first()
+    one=None
+    return {'one':one, 'project':'hi'}
+
+@view_config(route_name='contact', renderer="contact.mako")
+def contact_form(request):
+    
+    f = ContactForm(request.POST)   #empty form initializes if not a POST request
+    
+    if 'POST' == request.method and 'form.submitted' in request.params:
+        if f.validate():
+            #TODO: Do email sending here.
+            
+            request.session.flash("Your message has been sent!")
+            return HTTPFound(location=request.route_url('home'))
+    
+    return {'contact_form': f}
