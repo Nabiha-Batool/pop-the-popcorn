f1853c72327b52134ad1e82969b9129940cf2816
Nabiha Batool <great.nabiha@gmail.com>
2013-09-22 18:54:03 +0500

    removed mj

--- mj/mj/forms/info.py
+++
@@ -1,9 +0,0 @@
-from pyck.forms import Form
-from wtforms import SelectField, TextField, TextAreaField, validators
-
-
-class InfoForm(Form):
-    category_id = SelectField('Category', coerce=int)
-    first_name   = TextField('First Name', [validators.required("First name cannot be empty")])
-    description = TextAreaField("Description", [validators.required("Description cannot be empty")])
-
